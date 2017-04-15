80c167a24088d9f77dccb9ed1f0621dd79650754
Yash Shah <yashshah0127@gmail.com>
2017-04-15 10:36:44 -0700

    Add index and base html files

--- cooksmart/settings.py
+++ cooksmart/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = '-x#ko%g#ba3p^sn=wn!+1v80ymrm)ey__em9=wpn+u+-_rhjkw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
