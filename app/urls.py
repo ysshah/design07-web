8cb3749fec068543b023de3c0dc8d5c8dec04ea4
Yash Shah <yashshah0127@gmail.com>
2017-04-19 01:59:47 -0700

    Progress

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', RedirectView.as_view(pattern_name='calendar', permanent=False)),
+    url(r'^calendar/$', views.calendar, name='calendar'),
+    url(r'^recipes/$', views.recipes, name='recipes'),
+    url(r'^pantry/$', views.pantry, name='pantry'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,38 @@
+import calendar as pycal
+import datetime
+import boto3
+
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+recipesTable = dynamodb.Table('CookSmartRecipes')
+calendarTable = dynamodb.Table('CookSmartCalendar')
+pantryTable = dynamodb.Table('CookSmartCalendar')
+
+def calendar(request):
+    c = pycal.Calendar(6)
+    days = c.monthdayscalendar(2017, 4)
+
+    td = datetime.timedelta(1)
+    date = datetime.date.today()
+    context = {
+        'days': days,
+        'dates': [(date + td*i).strftime('%A, %B %d, %Y') for i in range(7)]
+    }
+    return render(request, 'calendar.html', context)
+
+def recipes(request):
+    items = recipesTable.scan()['Items']
+    mealTypes = []
+    for mealType in ['Breakfast', 'Lunch / Dinner']:
+        mealTypes.append(
+            (mealType, [r for r in items if r['MealType'] == mealType])
+        )
+    context = {
+        'mealTypes': mealTypes,
+        'thisRecipe': mealTypes[0][1][0]
+    }
+    return render(request, 'recipes.html', context)
+
+def pantry(request):
+    return render(request, 'pantry.html')
