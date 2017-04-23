c913b8be9509e0595d17a018ee5ae70158f997b5
Yash Shah <yashshah0127@gmail.com>
2017-04-22 21:16:13 -0700

    modularization and progress

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,12 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='calendar', permanent=False)),
     url(r'^calendar/$', views.calendar, name='calendar'),
     url(r'^recipes/$', views.recipes, name='recipes'),
+    url(r'^recipes/add/$', views.add_recipes, name='add_recipes'),
     url(r'^pantry/$', views.pantry, name='pantry'),
+    url(r'^pantry/add/$', views.add_ingredients, name='add_ingredients'),
 
+    # Ajax
     url(r'^view-recipe/$', views.view_recipe, name='view-recipe'),
+    url(r'^view-mealplan/$', views.view_mealplan, name='view-mealplan'),
+    url(r'^calendar-dates/$', views.calendar_dates, name='calendar-dates'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,24 +4,45 @@ import boto3
 
 from django.shortcuts import render
 
-
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 recipesTable = dynamodb.Table('CookSmartRecipes')
 calendarTable = dynamodb.Table('CookSmartCalendar')
 pantryTable = dynamodb.Table('CookSmartCalendar')
 
+cal = pycal.Calendar(6)
+
+
+def incrMonthYear(year, month, delta=1):
+    if month == 1 and delta == -1:
+        return year - 1, 12
+    elif month == 12 and delta == 1:
+        return year + 1, 1
+    return year, month + delta
+
 
 def calendar(request):
-    c = pycal.Calendar(6)
-    days = c.monthdayscalendar(2017, 4)
-
-    td = datetime.timedelta(1)
-    date = datetime.date.today()
+    today = datetime.date.today()
+    day = datetime.timedelta(1)
     context = {
-        'days': days,
-        'dates': [(date + td*i).strftime('%A, %B %d, %Y') for i in range(7)]
+        'dates': [(today + day*i).strftime('%A, %B %d, %Y') for i in range(7)]
     }
     return render(request, 'calendar.html', context)
+
+
+def calendar_dates(request):
+    date = datetime.datetime.strptime(request.POST['ym'], '%Y-%m')
+    day = datetime.timedelta(1)
+    context = {
+        'month_year': date.strftime('%B %Y'),
+        'prev': '{}-{:02d}'.format(*incrMonthYear(date.year, date.month, -1)),
+        'next': '{}-{:02d}'.format(*incrMonthYear(date.year, date.month, 1)),
+        'days': cal.monthdayscalendar(date.year, date.month)
+    }
+    return render(request, 'calendar_dates.html', context)
+
+
+def view_mealplan(request):
+    return render(request, 'view_mealplan.html')
 
 
 def recipes(request):
@@ -38,8 +59,16 @@ def recipes(request):
     return render(request, 'recipes.html', context)
 
 
+def add_recipes(request):
+    return render(request, 'add_recipes.html')
+
+
 def pantry(request):
     return render(request, 'pantry.html')
+
+
+def add_ingredients(request):
+    return render(request, 'add_ingredients.html')
 
 
 def view_recipe(request):
